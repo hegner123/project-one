42887f6e877c31918416e597148dbdb4d9d5cbf1
Michael Hegner <hegner123@gmail.com>
2019-09-26 14:58:08 -0400

    cleaned up code. Set api search back to limit 10

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -10,84 +10,84 @@ require([
 
   var map = new Map({
 
-      basemap: "streets"
+    basemap: "streets"
   });
   var view = new MapView({
-      container: "viewDiv",
-      map: map,
-      center: [-77.434769, 37.541290], // longitude, latitude
-      zoom: 11
+    container: "viewDiv",
+    map: map,
+    center: [-77.434769, 37.541290], // longitude, latitude
+    zoom: 11
   });
-  
+
 
   // ***************************************************************
   var search = new Search({
-      view: view
-    });
-    view.ui.add(search, "top-right"); // Add to the map
-          
-    // Find address
-    
-    view.on("click", function(evt){
-      search.clear(); 
-      view.popup.clear();
-      if (search.activeSource) {
-        var geocoder = search.activeSource.locator; // World geocode service
-        var params = {
-          location: evt.mapPoint 
-        };
-        geocoder.locationToAddress(params)
-          .then(function(response) { // Show the address found
-            var address = response.address;
-            showPopup(address, evt.mapPoint);
-          }, function(err) { // Show no address found
-            showPopup("No address found.", evt.mapPoint);
-          });
-      }
-    });
-    
-    function showPopup(address, pt) {
-      view.popup.open({
-        title:  + Math.round(pt.longitude * 100000)/100000 + ", " + Math.round(pt.latitude * 100000)/100000,
-        content: address,
-        location: pt
-      });
+    view: view
+  });
+  view.ui.add(search, "top-right"); // Add to the map
+
+  // Find address
+
+  view.on("click", function (evt) {
+    search.clear();
+    view.popup.clear();
+    if (search.activeSource) {
+      var geocoder = search.activeSource.locator; // World geocode service
+      var params = {
+        location: evt.mapPoint
+      };
+      geocoder.locationToAddress(params)
+        .then(function (response) { // Show the address found
+          var address = response.address;
+          showPopup(address, evt.mapPoint);
+        }, function (err) { // Show no address found
+          showPopup("No address found.", evt.mapPoint);
+        });
     }
+  });
+
+  function showPopup(address, pt) {
+    view.popup.open({
+      title: +Math.round(pt.longitude * 100000) / 100000 + ", " + Math.round(pt.latitude * 100000) / 100000,
+      content: address,
+      location: pt
+    });
+  }
   // ***************************************************************
 
   var basemapToggle = new BasemapToggle({
-      view: view,
-      nextBasemap: "streets-navigation-vector"
+    view: view,
+    nextBasemap: "streets-navigation-vector"
   })
   view.ui.add(basemapToggle, "bottom-right");
   var trailheadsLayer = new FeatureLayer({
-      url: "https://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/Landscape_Trees/FeatureServer/0"
+    url: "https://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/Landscape_Trees/FeatureServer/0"
   })
   map.add(trailheadsLayer);
   var point = {
-      type: "point",
-      longitude: -77.5387,
-      latitude: 37.5752,
+    type: "point",
+    longitude: -77.5387,
+    latitude: 37.5752,
   };
   var simpleMarkerSymbol = {
-      type: "simple-marker",
-      color: (0, 0, 255),
-      outline: {
-          color: [0, 0, 255],
-          width: .5
-      }
+    type: "simple-marker",
+    color: (0, 0, 255),
+    outline: {
+      color: [0, 0, 255],
+      width: .5
+    }
   };
-  var pointGraphic = new Graphic ({
-      geometry: point,
-      symbol: simpleMarkerSymbol
+  var pointGraphic = new Graphic({
+    geometry: point,
+    symbol: simpleMarkerSymbol
   });
   view.graphics.add(pointGraphic);
 });
 // -----------------------------------------------------------------------------------------------------------------------------------
-$(document).ready(function(){
+$(document).ready(function () {
   var userArray = [];
 
-  $("#add-options").on('click',function(){
+  $("#add-options").on('click', function () {
     var userInput = $("#options-add").val().trim();
     var output = $("<div>");
     output.text(userInput);
@@ -96,28 +96,22 @@ $(document).ready(function(){
     $("#options-add").val("");
   })
 
-  $("#reset").on("click",function(){
+  $("#reset").on("click", function () {
     userArray = [];
     $("#options-add").val("");
     $(".options-display").empty()
   })
 
-  function compare(x,y){
+  function compare(x, y) {
     return y.search(x);
   }
 
-  
-
-
-
-
-
   // zip search
-  $("#api-request").on("click", function(){
+  $("#api-request").on("click", function () {
     var input1 = $("#zip-search").val();
     var inputCheck = input1.toString();
     var idValue;
-    if (inputCheck.length === 5){
+    if (inputCheck.length === 5) {
       $("#display").empty();
       var settings = {
         "async": true,
@@ -130,65 +124,68 @@ $(document).ready(function(){
         }
       }
       $.ajax(settings)
-      .then(function (response) {
-        var results = response.result.data
-        console.log(results);
-        for (i=0;i<10;i++) {
-          console.log(results[i].cuisines)
-          var items = userArray.toString(", ");
-          var cuisine = results[i].cuisines
-          var display = true;
-          for (j=0;j<cuisine.length;j++){
-            if (compare(cuisine[j],items) === 0){
-              display = false;
-              console.log("fail");
+        .then(function (response) {
+          var results = response.result.data
+          console.log(results);
+          for (i = 0; i < 10; i++) {
+            console.log(results[i].cuisines)
+            var items = userArray.toString(", ");
+            var cuisine = results[i].cuisines
+            var display = true;
+            for (j = 0; j < cuisine.length; j++) {
+              if (compare(cuisine[j], items) === 0) {
+                display = false;
+                console.log("fail");
+              };
             };
-        };
-        if (display === true){
-          idValue = results[i].restaurant_id;
-          var idName = results[i].restaurant_name;
-          apiCall2 ( idValue, idName );
-        }
-        
-        input1 ="";
-        $("#zip-search").val("");
-      
-      }});
+            if (display === true) {
+              idValue = results[i].restaurant_id;
+              var idName = results[i].restaurant_name;
+              console.log(idName);
+              apiCall2(idValue, idName);
+            }
+
+            input1 = "";
+            $("#zip-search").val("");
+
+          }
+        });
     } else {
       $("#display").text("Please enter a zip code");
     };
   })
 })
 
-function apiCall2(idValue, restaurant_name){
-var settings2 = {
-  "async": true,
-  "crossDomain": true,
-  "url": "https://us-restaurant-menus.p.rapidapi.com/restaurant/" + idValue + "/menuitems?page=1",
-  "method": "GET",
-  "headers": {
-    "x-rapidapi-host": "us-restaurant-menus.p.rapidapi.com",
-    "x-rapidapi-key": "9b86b8e8a6mshdc12005b60b2e9bp17f3b5jsn2a7b1e24c609"
+function apiCall2(idValue, restaurant_name) {
+  var settings2 = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://us-restaurant-menus.p.rapidapi.com/restaurant/" + idValue + "/menuitems?page=1",
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-host": "us-restaurant-menus.p.rapidapi.com",
+      "x-rapidapi-key": "9b86b8e8a6mshdc12005b60b2e9bp17f3b5jsn2a7b1e24c609"
+    }
   }
-}
-  $.ajax(settings2).then( function (response2) {
-  var menuResults = response2.result.data;
-  var resultText = "Restaurant:" + restaurant_name + "|| Menu Item: " + menuResults[0].menu_item_name;
-  var option = $('<div>');
-  option.text(resultText);
-  option.appendTo("#display");
-})
+  $.ajax(settings2).then(function (response2) {
+    var menuResults = response2.result.data;
+    console.log(menuResults);
+    var resultText = "Restaurant:" + restaurant_name + "|| Menu Item: " + menuResults[0].menu_item_name;
+    var option = $('<div>');
+    option.text(resultText);
+    option.appendTo("#display");
+  })
 };
 
 
 
-function menuFields(){
+function menuFields() {
 
-var fields = {
-  name:"cheeseburger"
-};
+  var fields = {
+    name: "cheeseburger"
+  };
 
-var paramaters = $.param( fields );
+  var paramaters = $.param(fields);
 
 
   console.log("menuFields Launch")
@@ -202,12 +199,9 @@ var paramaters = $.param( fields );
       "x-rapidapi-key": "9b86b8e8a6mshdc12005b60b2e9bp17f3b5jsn2a7b1e24c609"
     }
   }
-  
+
   $.ajax(settings).done(function (response) {
     console.log(response);
     console.log("-----------------------------------------------------------------------")
   });
 }
-
-
-
